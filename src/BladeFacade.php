f5f66eac1f4736fa9a70a78a19e1fca6a25f6494
Oskar Joelson <oskar.joelson@sot.se>
2019-04-01 09:53:02 +0200

    Simplify __callStatic

--- src/BladeFacade.php
+++ src/BladeFacade.php
@@ -33,18 +33,10 @@ class BladeFacade
      * @param  string  $method
      * @param  array   $args
      * @return mixed
-     *
-     * @throws \RuntimeException
      */
-    public static function __callStatic($method, $args)
+    public static function __callStatic(string $method, array $args)
     {
-        $instance = self::instance();
-
-        if (!$instance) {
-            throw new \RuntimeException('A facade root has not been set.');
-        }
-
-        return $instance->$method(...$args);
+        return self::instance()->$method(...$args);
     }
 
     /**
