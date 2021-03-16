bd461c536b0c21dfe67de96b0d1126eb20fefb8d
fiskhandlarn <oskar@joelson.org>
2021-03-16 10:05:25 +0100

    Add copyright notices, declare strict

--- src/BladeFacade.php
+++ src/BladeFacade.php
@@ -1,5 +1,24 @@
 <?php
 
+/**
+ * This file is part of blade.
+ *
+ * blade is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * blade is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with blade.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
+declare(strict_types=1);
+
 namespace Fiskhandlarn;
 
 use Fiskhandlarn\Blade;
--- tests/BladeTest.php
+++ tests/BladeTest.php
@@ -1,12 +1,20 @@
 <?php
 
-/*
- * This file is part of fiskhandlarn/blade.
+/**
+ * This file is part of blade.
  *
- * (c) Oskar Joelson <oskar@joelson.org>
+ * blade is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
  *
- * For the full copyright and license information, please view the LICENSE
- * file that was distributed with this source code.
+ * blade is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with blade.  If not, see <https://www.gnu.org/licenses/>.
  */
 
 declare(strict_types=1);
